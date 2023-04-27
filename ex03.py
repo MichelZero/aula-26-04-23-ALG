1eda2bcee2184b75142145011763f63524bb7c4e
Michel <106674127+MichelZero@users.noreply.github.com>
2023-04-27 08:55:54 -0300

    up

---
+++ ex03.py
@@ -0,0 +1,19 @@
+#
+#
+# 1 -  Escreva uma função que recebe dois parâmetros e imprime o menor dos dois.
+#
+def somar(valor1, valor2):
+  soma = valor1 + valor2
+  return soma
+
+def subt(valor1, valor2):
+  subtrair = valor1 - valor2
+  return subtrair
+
+def mult(valor1, valor2):
+  multiplicar = valor1 - valor2
+  return multiplicar
+
+num1 = int(input("informe um valor1: "))
+num2 = int(input("informe um valor2: "))
+print(funcao_teste(num1,num2))
---
+++ ex04.py
@@ -0,0 +1,11 @@
+#
+def somar(valor1, valor2):
+  resultado = valor1 + valor2
+  return resultado
+
+def somar2(valor1, valor2):
+  resultado = valor1 + valor2
+  print(resultado)
+
+print(somar(2,2))
+somar2(2,2)
